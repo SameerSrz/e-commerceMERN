44b4d2b76ddd0390ed5f22f945e157aa5cd577ff
Sameer Riaz <muhammad.sameer@purelogics.net>
2023-10-05 22:02:58 +0500

    features!!

---
+++ src/Components/index.js
@@ -0,0 +1,21 @@
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import Subscribe from "./Subscribe";
+import CustomerReviews from "./CustomerReviews";
+import Hero from "./Hero";
+import Services from "./Services";
+import PopularProducts from "./PopularProducts";
+import SpecialOffer from "./SpecialOffer";
+import SuperQuality from "./SuperQuality";
+
+export {
+  Navbar,
+  Footer,
+  Subscribe,
+  CustomerReviews,
+  Hero,
+  Services,
+  PopularProducts,
+  SpecialOffer,
+  SuperQuality,
+};
---
+++ src/Constants/index.js
@@ -0,0 +1,67 @@
+import {
+  facebook,
+  instagram,
+  shieldTick,
+  support,
+  truckFast,
+  twitter,
+} from "../assets/icons";
+import {
+  bigShoe1,
+  bigShoe2,
+  bigShoe3,
+  customer1,
+  customer2,
+  shoe4,
+  shoe5,
+  shoe6,
+  shoe7,
+  thumbnailShoe1,
+  thumbnailShoe2,
+  thumbnailShoe3,
+} from "../assets/images";
+
+export const navLinks = [
+  { href: "#home", label: "Home" },
+  { href: "#about-us", label: "About Us" },
+  { href: "#products", label: "Products" },
+  { href: "#contact-us", label: "Contact Us" },
+];
+
+export const shoes = [
+  {
+    thumbnail: thumbnailShoe1,
+    bigShoe: bigShoe1,
+  },
+  {
+    thumbnail: thumbnailShoe2,
+    bigShoe: bigShoe2,
+  },
+  {
+    thumbnail: thumbnailShoe3,
+    bigShoe: bigShoe3,
+  },
+];
+
+export const statistics = [
+  { value: "1k+", label: "Brands" },
+  { value: "500+", label: "Shops" },
+  { value: "250k+", label: "Customers" },
+];
+
+export const products = [
+  {
+    imgURL: shoe4,
+    name: "Nike Air Jordan-01",
+    price: "$200.20",
+  },
+  {
+    imgURL: shoe5,
+    name: "Nike Air Jordan-10",
+    price: "$210.20",
+  },
+  {
+    imgURL: shoe6,
+    name: "Nike Air Jordan-100",
+  },
+];
